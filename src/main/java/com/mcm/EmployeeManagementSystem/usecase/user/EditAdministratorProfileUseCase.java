1c666625d1ae652b781e867f1c345d7623ddfe8b
MarkoCvijeticRA40 <93485029+MarkoCvijeticRA40@users.noreply.github.com>
2023-05-24 14:06:44 +0200

    Reformat code

---
+++ src/main/java/com/mcm/EmployeeManagementSystem/usecase/user/EditAdministratorProfileUseCase.java
@@ -0,0 +1,33 @@
+package com.mcm.EmployeeManagementSystem.usecase.user;
+
+import com.mcm.EmployeeManagementSystem.model.User;
+import com.mcm.EmployeeManagementSystem.store.UserStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EditAdministratorProfileUseCase {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserStore userStore;
+
+    public User updateAdministrator(User administrator) {
+        if(IsAdministratorPasswordChanged(administrator.getPassword(),administrator.getId()) == true  )
+        {
+            administrator.setPassword(passwordEncoder.encode(administrator.getPassword()));
+            return userStore.save(administrator);
+        }
+        return userStore.save(administrator);
+    }
+
+    public boolean IsAdministratorPasswordChanged(String password,Long id) {
+        User administrator = userStore.getById(id);
+        if(administrator.getPassword().equals(password)) {
+            return false;
+        }
+        return true;
+    }
+}
